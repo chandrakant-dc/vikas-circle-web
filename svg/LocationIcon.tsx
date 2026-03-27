89a3d331790278740e25e03de7dd78cb5d7c4bbf
chandrakant-dc <chandrakant@developmentcart.com>
2026-03-27 11:30:19 +0530

    FEAT : contact page ui

---
+++ svg/LocationIcon.tsx
@@ -0,0 +1,35 @@
+
+export default function LocationIcon() {
+    return <svg
+        xmlns="http://www.w3.org/2000/svg"
+        width="64"
+        height="60"
+        fill="none"
+        viewBox="0 0 64 60"
+    >
+        <title>Location</title>
+        <rect
+            width="64"
+            height="60"
+            fill="url(#paint0_linear_2011_509)"
+            rx="10"
+        ></rect>
+        <path
+            fill="#fff"
+            d="M32 15c-5.797 0-10.5 4.703-10.5 10.5C21.5 33.375 32 45 32 45s10.5-11.625 10.5-19.5C42.5 19.703 37.797 15 32 15m0 14.25c-2.07 0-3.75-1.68-3.75-3.75s1.68-3.75 3.75-3.75 3.75 1.68 3.75 3.75-1.68 3.75-3.75 3.75"
+        ></path>
+        <defs>
+            <linearGradient
+                id="paint0_linear_2011_509"
+                x1="0"
+                x2="64"
+                y1="30"
+                y2="30"
+                gradientUnits="userSpaceOnUse"
+            >
+                <stop stopColor="#4F29ED"></stop>
+                <stop offset="1" stopColor="#9D00FF"></stop>
+            </linearGradient>
+        </defs>
+    </svg>
+}
